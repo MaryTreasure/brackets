a0b1a3583a097fb9a726c482f03bb12ed347520d
MaryTreasure <mari199191@mail.ru>
2024-08-04 20:24:37 +0300

    feat: implement check function for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrFromStr = str.split('');
+
+  for(let i = 0; i < arrFromStr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if(arrFromStr[i] === bracketsConfig[j][0] && arrFromStr[i + 1] === bracketsConfig[j][1]) {
+        arrFromStr.splice(i, 2);
+        i -= 2
+      }
+    }
+  }
+
+  return !arrFromStr.length;
 }
